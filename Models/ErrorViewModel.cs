08c5ffd456e64feabb0308f3a89edc3e4fd55b9f
FelixDaDalt <felixdadalt@gmail.com>
2022-06-15 14:57:30 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_DevChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
